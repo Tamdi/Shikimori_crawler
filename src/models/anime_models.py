a1c6f68159ef471af4e09cf1ea1d4cdd63b50339
tamdi <tamyrlan.daniyaruly@gmail.com>
2022-01-21 01:41:45 +0600

    update anime_models

--- src/models/anime_models.py
+++ src/models/anime_models.py
@@ -4,45 +4,45 @@ from typing import Optional, List
 
 class Character(BaseModel):
     url: str
-    id: List[str]
-    name: List[str]
-    name_rus: List[str]
-    image_url: List[str]
+    id: Optional[List[str]]
+    name: Optional[List[str]]
+    name_rus: Optional[List[str]]
+    image_url: Optional[List[str]]
 
 
 class Staff(BaseModel):
     url: str
     id: List[str]
-    name: List[str]
-    name_rus: List[str]
-    occupation: List[str]
-    image_url: List[str]
+    name: Optional[List[str]]
+    name_rus: Optional[List[str]]
+    occupation: Optional[List[str]]
+    image_url: Optional[List[str]]
 
 
 class Studio(BaseModel):
     url: str
     id: str
-    name: str
-    image_url: str
+    name: Optional[str]
+    image_url: Optional[str]
 
 
 class Anime(BaseModel):
     url: str
     id: str
     name: str
-    name_rus: str
-    name_alt: str
-    type: str
-    total_episodes: int
-    current_episodes: str
-    next_episode_date: str
-    started: str
+    name_rus: Optional[str]
+    name_alt: Optional[str]
+    type: Optional[str]
+    total_episodes: Optional[int]
+    current_episodes: Optional[str]
+    next_episode_date: Optional[str]
+    started: Optional[str]
     genres: Optional[str]
-    score: float
-    rating: str
-    licensed_by: str
-    studio: List[Studio]
-    description: str
+    score: Optional[float]
+    rating: Optional[str]
+    licensed_by: Optional[str]
+    studio: Optional[List[Studio]]
+    description: Optional[str]
     related: Optional[List[str]]
     author: Optional[List[Staff]]
     main_heroes: Optional[List[Character]]
